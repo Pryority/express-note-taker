94ecc11bc9be21ae1a6ee7cde94adcb8bbdcd792
Matthew Pryor <matthewapryor@gmail.com>
2022-05-08 11:39:35 -0400

    add htmlRoutes

--- server.js
+++ server.js
@@ -1,23 +1,24 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
-// const { noteData } = require('./db/db.json');
+const { noteData } = require('./db/db.json');
 // const apiRoutes = require('./routes/apiRoutes');
-// const htmlRoutes = require('./routes/htmlRoutes');
+const htmlRoutes = require('./public/routes/htmlRoutes');
 // const res = require('express/lib/response');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.static('public'));
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// app.use('/api', apiRoutes);
-// app.use('/', htmlRoutes);
+app.use('/', htmlRoutes);
 
-app.get('/api', (req, res) => {
-    res.send('Hello');
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
 })
-
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+})
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);

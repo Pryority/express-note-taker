a2b7e5fc3d7713b0beb983bfa48da1e9f78eeff9
Matthew Pryor <matthewapryor@gmail.com>
2022-05-08 23:50:32 -0400

    add reminder for Jest mock

--- routes/apiRoutes/notes.js
+++ routes/apiRoutes/notes.js
@@ -14,7 +14,8 @@ router.get("/notes", (req, res) => {
 //         res.send(404);
 //     }
 // });
-
+// TODO: add Jest mock to prevent db.json from being rewritten every time you POST.
+// jest.mock();
 router.post('/notes', (req, res) => {
     // set id based on what the next index of the array will be
     req.body.id = notes.length.toString();

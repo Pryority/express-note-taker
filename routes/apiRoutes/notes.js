e245be6e299956435e839facac658e48d61ddc7f
Matthew Pryor <matthewapryor@gmail.com>
2022-05-08 22:31:40 -0400

    POST request now adds new note to db with ID

--- routes/apiRoutes/notes.js
+++ routes/apiRoutes/notes.js
@@ -15,17 +15,31 @@ router.get("/notes", (req, res) => {
 //     }
 // });
 
-router.post("/notes", (req, res) => {
+router.post('/notes', (req, res) => {
     // set id based on what the next index of the array will be
-    // req.body.id = notes.length.toString();
+    req.body.id = notes.length.toString();
 
-    // if any data in req.body is incorrect, send 400 error back
-    if (!validateNote(req.body)) {
-        res.status(400).send("That note is not properly formatted!");
-    } else {
-        const note = createNewNote(req.body, notes);
-        res.json(note);
-    }
+    // add note to json file and notes array in this function
+    const note = createNewNote(req.body, notes);
+
+    res.json(note);
 });
+
+// router.post("/notes", (req, res) => {
+//     // set id based on what the next index of the array will be
+//     // req.body.id = notes.length.toString();
+
+//     // if any data in req.body is incorrect, send 400 error back
+//     // if (!validateNote(req.body)) {
+//     //     res.status(400).send("That note is not properly formatted!");
+//     // } else {
+//     //     const note = createNewNote(req.body, notes);
+//     //     res.json(note);
+//     // }
+
+//     // req.body is where our incoming content will be
+//     console.log(req.body);
+//     res.json(req.body);
+// });
 
 module.exports = router;

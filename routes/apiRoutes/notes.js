51967e3371fb40975ef96c3bdf3528be24e5a120
pryority <matthewapryor@gmail.com>
2022-07-15 14:27:11 -0400

    can add and delete notes

--- routes/apiRoutes/notes.js
+++ routes/apiRoutes/notes.js
@@ -1,13 +1,24 @@
 const router = require("express").Router();
-const { filterById, validateNote, createNewNote } = require("../../lib/notes.js");
-const notes = require("../../db/db.json");
 
+const {
+    filterById,
+    createNewNote,
+    validateNote,
+    deleteNote,
+} = require("../../lib/notes");
+
+const { note } = require("../../db/db.json");
+
+// returns the JSON formatted database
 router.get("/notes", (req, res) => {
-    res.json(notes);
+    let results = note;
+    res.json(results);
 });
 
+// returns a specific note in a JSON format
 router.get("/notes/:id", (req, res) => {
-    const result = filterById(req.params.id, notes);
+    let specificNote = req.params.id;
+    const result = filterById(specificNote, note);
     if (result) {
         res.json(result);
     } else {
@@ -15,44 +26,25 @@ router.get("/notes/:id", (req, res) => {
     }
 });
 
-router.post('/notes', (req, res) => {
-    // set id based on what the next index of the array will be
-    req.body.id = notes.length.toString();
-
-    // add note to json file and notes array in this function
-    const note = createNewNote(req.body, notes);
-
-    res.json(note);
-
+// allows a user to add new note to JSON database
+router.post("/notes", (req, res) => {
     let newNote = req.body;
-    let noteList = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-    let notelength = (noteList.length).toString();
-
-    //create new property called id based on length and assign it to each json object
-    newNote.id = notelength;
-    //push updated note to the data containing notes history in db.json
-    noteList.push(newNote);
-
-    //write the updated data to db.json
-    fs.writeFileSync("./db/db.json", JSON.stringify(noteList));
-    res.json(noteList);
+    if (note.length === 0) {
+        newNote.id = "0";
+    } else {
+        newNote.id = note.length.toString();
+    }
+    if (!validateNote(newNote)) {
+        res.status(400).send("This note is not properly formatted.");
+    } else {
+        const noteData = createNewNote(newNote, note);
+        res.json(noteData);
+    }
 });
 
-// router.post("/notes", (req, res) => {
-//     // set id based on what the next index of the array will be
-//     // req.body.id = notes.length.toString();
-
-//     // if any data in req.body is incorrect, send 400 error back
-//     // if (!validateNote(req.body)) {
-//     //     res.status(400).send("That note is not properly formatted!");
-//     // } else {
-//     //     const note = createNewNote(req.body, notes);
-//     //     res.json(note);
-//     // }
-
-//     // req.body is where our incoming content will be
-//     console.log(req.body);
-//     res.json(req.body);
-// });
+router.delete("/notes/:id", (req, res) => {
+    let newNoteArray = deleteNote(req.params.id, note);
+    res.json(newNoteArray);
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const fs = require('fs');
-const path = require('path');
 const express = require('express');
-const { notes } = require('./db/db.json');
-const apiRoutes = require('./routes/apiRoutes/notes.js');
-const htmlRoutes = require('./routes/htmlRoutes/index.js');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -15,6 +13,4 @@ app.use(express.json());
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
-app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
-});
+app.listen(PORT, () => { console.log(`API server now on port ${PORT}!`); });
